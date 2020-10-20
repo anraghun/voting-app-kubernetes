6629ee7d31fa125bb0a10bcbb1c43ce1715456c1
Anirudh Raghunath <anraghun@github.com>
2020-10-20 19:40:47 +0530

    Added worker deployment workflow

---
+++ worker/scripts/copyArtifacts.sh
@@ -0,0 +1,2 @@
+mkdir -p /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow-artifacts/
+cp -R /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/worker/* /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow-artifacts/
