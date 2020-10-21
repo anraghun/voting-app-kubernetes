699650e1ab0279e8df1be887402d517b801ffb3d
Anirudh Raghunath <anraghun@github.com>
2020-10-21 16:19:47 +0530

    added logging to copy script

--- worker/scripts/copyArtifacts.sh
+++ worker/scripts/copyArtifacts.sh
@@ -1,2 +1,10 @@
+echo "Copying artifacts ..."
+
 mkdir -p /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow-artifacts/
 cp -R /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/worker/* /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow-artifacts/
+
+echo "Finished copying artifacts"
+
+cd /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow-artifacts/
+pwd
+ls -ltr
