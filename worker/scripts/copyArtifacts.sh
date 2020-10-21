5bf45e0a59a84570f14ddcbe579827475093b0f5
Anirudh Raghunath <anraghun@github.com>
2020-10-21 16:31:33 +0530

    fixed copy path

--- worker/scripts/copyArtifacts.sh
+++ worker/scripts/copyArtifacts.sh
@@ -1,10 +1,10 @@
 echo "Copying artifacts ..."
 
-mkdir -p /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow-artifacts/
-cp -R /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/worker/* /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow-artifacts/
+mkdir -p /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow_artifacts/
+cp -R /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/worker/* /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow_artifacts/
 
 echo "Finished copying artifacts"
 
-cd /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow-artifacts/
+cd /home/runner/work/voting-app-kubernetes/voting-app-kubernetes/workflow_artifacts/
 pwd
 ls -ltr
